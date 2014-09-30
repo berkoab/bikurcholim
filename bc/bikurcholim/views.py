eaea0e3e0c781600dd6afe7857a0f9f850d394fd
berkoab <aryeh.berkowitz@gmail.com>
2014-09-29 22:31:38 -0400

    works

--- bc/bikurcholim/views.py
+++ bc/bikurcholim/views.py
@@ -5,6 +5,7 @@ from bikurcholim.models import Clients
 from django.core import serializers
 import re
 import json
+import collections
 
 def index(request):
     return HttpResponse("Hello, world. You're at the bikurcholim index.")
@@ -14,7 +15,7 @@ def volunteers(request):
 	cols['id']={
 		'index': 1, #The order this column should appear in the table
 		'type': "number", #The type. Possible are string, number, bool, date(in milliseconds).
-		'friendly': "<span class='glyphicon glyphicon-user'></span>",  #Name that will be used in header. Can also be any html as shown here.
+		'friendly': "id",  #Name that will be used in header. Can also be any html as shown here.
 		'format': "<a href='#' class='userId' target='_blank'>{0}</a>",  #Used to format the data anything you want. Use {0} as placeholder for the actual data.
 		'unique': 'true',  #This is required if you want checkable rows, or to use the rowClicked callback. Be certain the values are really unique or weird things will happen.
 		'sortOrder': "asc", #Data will initially be sorted by this column. Possible are "asc" or "desc"
@@ -28,7 +29,7 @@ def volunteers(request):
 		'tooltip': "This column has a custom placeholder", #Show some additional info about column
 		'placeHolder': "abc123" #Overrides default placeholder and placeholder specified in data types(row 34).
 	}
-	
+
 	rows=[]
 	
 	#data = Volunteers.objects.all()
@@ -37,11 +38,15 @@ def volunteers(request):
 	#	re.sub(r'(datetime.time\()(\d+)(,)(\d+)(, )(\d+)(\))', r'\2\\4\\6', item)
 	data = serializers.serialize("json", Volunteers.objects.all())		
 	j = json.loads(data)
-	for item in j:
-		rows.append(item['fields'])
+	#for item in j:
+	#	rows.append(item['fields'])
 	
-	
-	r = {}
+	d = Volunteers.objects.all()
+	columns = {}
+	columns['id']=d[0].id
+	columns['name']=d[0].first_name
+	rows.append(columns)
+	r = collections.OrderedDict()
 	r['cols'] = cols
 	r['rows'] = rows
 	
