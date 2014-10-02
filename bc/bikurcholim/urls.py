c95e078ef34ee46feb2d52b4d9f21b9018e56f11
berkoab <aryeh.berkowitz@gmail.com>
2014-10-02 12:01:47 -0400

    detail view

--- bc/bikurcholim/urls.py
+++ bc/bikurcholim/urls.py
@@ -7,7 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
 	url(r'^volunteers/$', views.volunteers, name='volunteers'),
 	url(r'^clients/$', views.clients, name='clients'),
-	#url(r'^volunteers/(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
 	url(r'^volunteers/(?P<pk>\d+)/$', VolunteersDetailView.as_view(), name='article-detail'),
 	url(r'^login/$', views.user_login, name='login'),
 )
--- bc/bikurcholim/views.py
+++ bc/bikurcholim/views.py
@@ -232,42 +232,48 @@ def clients(request):
 	return render(request, 'bikurcholim/clients.html', context)
 
 def user_login(request):
-    # Like before, obtain the context for the user's request.
-    context = RequestContext(request)
+	# Like before, obtain the context for the user's request.
+	context = RequestContext(request)
 
-    # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
-        username = request.POST['username']
-        password = request.POST['password']
+	# If the request is a HTTP POST, try to pull out the relevant information.
+	if request.method == 'POST':
+		# Gather the username and password provided by the user.
+		# This information is obtained from the login form.
+		username = request.POST['username']
+		password = request.POST['password']
+		nextpost = request.POST['next']
+		# Use Django's machinery to attempt to see if the username/password
+		# combination is valid - a User object is returned if it is.
+		user = authenticate(username=username, password=password)
 
-        # Use Django's machinery to attempt to see if the username/password
-        # combination is valid - a User object is returned if it is.
-        user = authenticate(username=username, password=password)
+		# If we have a User object, the details are correct.
+		# If None (Python's way of representing the absence of a value), no user
+		# with matching credentials was found.
+		if user:
+			# Is the account active? It could have been disabled.
+			if user.is_active:
+				# If the account is valid and active, we can log the user in.
+				# We'll send the user back to the homepage.
+				login(request, user)
+				if len(nextpost) > 0:
+					return HttpResponseRedirect(nextpost)
+				else:
+					return HttpResponseRedirect('bikurcholim')
+			else:
+				# An inactive account was used - no logging in!
+				return HttpResponse("Your Bikur Cholim account is disabled.")
+		else:
+			# Bad login details were provided. So we can't log the user in.
+			print "Invalid login details: {0}, {1}".format(username, password)
+			return HttpResponse("Invalid login details supplied.")
 
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching credentials was found.
-        if user:
-            # Is the account active? It could have been disabled.
-            if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage.
-                login(request, user)
-                return HttpResponseRedirect(request.POST['next'])
-            else:
-                # An inactive account was used - no logging in!
-                return HttpResponse("Your Bikur Cholim account is disabled.")
-        else:
-            # Bad login details were provided. So we can't log the user in.
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
-
-    # The request is not a HTTP POST, so display the login form.
-    # This scenario would most likely be a HTTP GET.
-    else:
-        # No context variables to pass to the template system, hence the
-        # blank dictionary object...
-        return render_to_response('bikurcholim/login.html', {}, context)
-
+	# The request is not a HTTP POST, so display the login form.
+	# This scenario would most likely be a HTTP GET.
+	else:
+		# No context variables to pass to the template system, hence the
+		# blank dictionary object...
+		next = request.GET.get('next')
+		context = {
+			'next': next,
+		}
+		return render(request, 'bikurcholim/login.html', context)
