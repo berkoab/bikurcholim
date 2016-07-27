9ca367b44efcf54ff1a6426484a2ec162921717f
berkoab <aryeh.berkowitz@gmail.com>
2016-07-27 14:20:36 +0000

    updates urls

--- bc/urls.py
+++ bc/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from model_report import report
-report.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
