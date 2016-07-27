55ce055e20fb9edaa0ad33bd44d580751d760751
berkoab <aryeh.berkowitz@gmail.com>
2016-07-27 14:17:45 +0000

    updates urls

--- bc/urls.py
+++ bc/urls.py
@@ -6,7 +6,6 @@ report.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bc.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', include('bikurcholim.urls')),
     url(r'^bikurcholim/', include('bikurcholim.urls')),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
